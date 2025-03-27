df7ba2310dcd26a5bbe2dd70170d05b09d9cc614
Joseph <underradicals@gmail.com>
2025-03-27 00:18:14 -0500

    Add Serilog to the project Add Structured Logging for the File Sink Add Non-Structured Logging for the Console Sink Add WeatherForecastEndpoint for testing Logging
    Note:
      Did not add global or centralized logging for the project:
        - I am using FastEndpoints for the API and they offer Post/Pre
        middleware for logging.
        - This eliminates the need for a global logger.
    
    On branch feature/add_serilog
    Your branch is up to date with 'origin/feature/add_serilog'.
    Changes to be committed:
    	modified:   Destiny2App.ClientApi.csproj
    	new file:   Endpoints/WeatherForecastEndpoint.cs
    	modified:   Program.cs
    	modified:   appsettings.Development.json
    	modified:   ../Destiny2App.Infrastructure/Destiny2App.Infrastructure.csproj
    	new file:   ../Destiny2App.Infrastructure/Infrastructure_DI_Extensions.cs

---
+++ src/Destiny2App.ClientApi/Endpoints/WeatherForecastEndpoint.cs
@@ -0,0 +1,35 @@
+﻿using FastEndpoints;
+
+namespace Destiny2App.ClientApi.Endpoints;
+
+public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+public class WeatherForecastEndpoint : EndpointWithoutRequest<WeatherForecast[]>
+{
+    public readonly string[] summaries =
+    [
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    ];
+    
+    public override void Configure()
+    {
+        Get("/weatherforecast");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(CancellationToken cancellationToken)
+    {
+        var forecasts = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+            .ToArray();
+        await SendAsync(forecasts, cancellation: cancellationToken);
+    }
+}
